509594026f3dfc3f463324f2fed08c79b72a00b5
Kevin Adameit <kevin.adameit@icloud.com>
2024-11-23 23:00:45 +0100

    Added and fixed chat endpoint validation

--- backend/chats/forms.py
+++ backend/chats/forms.py
@@ -1,38 +1,62 @@
 from typing import Any
 from django import forms
 from django.core.exceptions import ValidationError
+from rest_framework.authtoken.admin import User
 
-from backend.offers.models import Chat, Message
+from backend.offers.models import Chat, Message, Offer
 
 
 class CreateMessageForm(forms.Form):
     chat = forms.UUIDField()
     author = forms.UUIDField()
-
     content = forms.CharField(max_length=255)
-
     created_at = forms.DateTimeField(auto_now_add=True)
+
+    def validate_chat(self):
+        try:
+            Chat.objects.get(identifier=self.cleaned_data['chat'])
+        except Chat.DoesNotExist:
+            raise forms.ValidationError('Invalid chat.')
+
+    def validate_author(self):
+        try:
+            User.objects.get(identifier=self.cleaned_data['author'])
+        except User.DoesNotExist:
+            raise forms.ValidationError('Invalid author.')
 
     def create_msg(self, author):
         content = self.cleaned_data['content']
         chat = Chat.objects.get(identifier=self.cleaned_data['chat'])
 
-        return Message.objects.create(chat=chat, author=author, content=content)
+        if chat.author != author and chat.offer.author != author:
+            raise PermissionError('You are not permitted to write to this chat')
+
+        return Message.objects.create(chat=chat.identifier, author=author, content=content)
 
 class CreateChatForm(forms.Form):
     offer = forms.UUIDField()
-    author = forms.UUIDField()
+
+    def validate_offer(self):
+        try:
+            Offer.objects.get(identifier=self.cleaned_data['offer'])
+        except Offer.DoesNotExist:
+            raise forms.ValidationError('Invalid offer.')
 
     def create_chat(self, author):
         offer = self.cleaned_data['offer']
-
         return Chat.objects.create(author=author, offer=offer)
 
 class GetMessagesForm(forms.Form):
     chat = forms.UUIDField()
 
+    def validate_chat(self):
+        try:
+            Chat.objects.get(identifier=self.cleaned_data['chat'])
+        except Chat.DoesNotExist:
+            raise forms.ValidationError('Invalid chat.')
+
     def get_messages(self, user):
-        chat = Chat.objects.get(identifier=self.cleaned_data['identifier'])
+        chat = Chat.objects.get(identifier=self.cleaned_data['chat'])
 
         if user != chat.author and user != chat.offer.author:
             raise PermissionError('You are not permitted to read this chat.')
