dd30584815da7f691ee0d46127a7b1aa5bc42a19
Christoph Gerards <christoph.gerards@gmail.com>
2024-11-23 15:07:28 +0100

    React axios api inclusion

--- frontend/src/pages/Offers.js
+++ frontend/src/pages/Offers.js
@@ -1,12 +1,26 @@
 import '../App.css';
+import '../api.js'
+
+
 
 function Offers() {
+
+    const fetchResponse = async () => {
+        const response = await api.get('/index');
+        console.log(response);
+    }
+
+
+
     return (
       <div className="Offers">
         <header className="App-header">
           <p>
           Offers example page!!!
           </p>
+
+
+          <h2> {fetchResponse()} </h2>
           <a
             className="App-link"
             href="https://reactjs.org"
