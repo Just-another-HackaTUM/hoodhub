a8a2a958c4048f3c4831a9e5487a7690a8073f08
Christoph Gerards <christoph.gerards@gmail.com>
2024-11-23 14:17:59 +0100

    React basic structure for multipaging and seamless loading

---
+++ frontend/src/pages/Offers.js
@@ -0,0 +1,23 @@
+import '../App.css';
+
+function Offers() {
+    return (
+      <div className="Offers">
+        <header className="App-header">
+          <p>
+          Offers example page!!!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Offers example page!!!
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Offers;
