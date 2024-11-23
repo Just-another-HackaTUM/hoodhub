ba21198682a9392db4f263417bb33eb4da4a866a
Christoph Gerards <christoph.gerards@gmail.com>
2024-11-23 18:38:49 +0100

    Login and Register in frontend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,23 +1,52 @@
-import '../App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function Login() {
+import '../App.css';
+import api from '../api.js'
+
+
+
+function Login({ currentUser, setCurrentUser, email, username, password, setEmail, setUsername, setPassword }) {
+
+
+    function submitLogin(e) {
+        e.preventDefault();
+        api.post(
+            '/login/',
+            {
+                email: email,
+                password: password
+            }
+        ).then(function (res) {
+            setCurrentUser(true);
+        });
+    }
+
+
+
+
     return (
-      <div className="Offers">
-        <header className="App-header">
-          <p>
-          Login example page!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Login example page!!!
-          </a>
-        </header>
-      </div>
+        <div className="center">
+            <Form onSubmit={e => submitLogin(e)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
     );
-  }
-  
-  export default Login;
+}
+
+export default Login;
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,29 @@
+import '../App.css';
+
+import api from '../api.js' 
+
+
+
+
+
+function Register({ currentUser, setCurrentUser, email, username, password, setEmail, setUsername, setPassword }) {
+    return (
+      <div className="Offers">
+        <header className="App-header">
+          <p>
+          Register example page!!!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Login example page!!!
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Register;
